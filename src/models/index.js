1b97f27958a9d018cedecd70ac78b26f32ef1390
cordovabarbara <aliendocordova@gmail.com>
2023-06-19 08:20:17 -0500

    finish project

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+const EmailCode = require("./EmailCode");
+const User = require("./User");
+
+
+EmailCode.belongsTo(User)//userId
+User.hasOne(EmailCode)
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routerUser = require('./user.router');
+const router = express.Router();
+
+// colocar las rutas aquí
+
+router.use('/users', routerUser)
+router.use('/verify/:code', routerUser)
+router.use('/verify/:code', routerUser)
+router.use('/reset_password', routerUser)
+
+
+
+module.exports = router;
